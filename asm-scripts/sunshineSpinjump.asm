14a3ecdcd54572633f7a68114af41ffd2f8568ff
Ryan Stillings <rystills@gmail.com>
2018-02-14 22:47:00 -0500

    reimplement legacy spinjump code using function with much larger area

--- asm-scripts/sunshineSpinjump.asm
+++ asm-scripts/sunshineSpinjump.asm
@@ -1,41 +1,32 @@
 .defineLabel MARIO_STRUCT, 0x8033B170
 
 ; See this wiki page for a list of Mario's actions: http://wiki.origami64.net/sm64:actions
-.defineLabel ACTION_DIVE, 0x0188088A 
-.defineLabel ACTION_SLIDE, 0x00880456
-.defineLabel ACTION_SLIDERECOVER, 0x00000386
-.defineLabel ACTION_SLOPESLIDE, 0x00880453
+.defineLabel ACTION_FALL, 0x0100088C
+.defineLabel ACTION_MOVING, 0x04000440
 
 ; begin function
-.orga 0xc7834
-.area 0xFF
+.orga 0xc7834 ; Set ROM address, we are overwritting a useless loop function as our hook.
+.area 0xFF ; Set data import limit to 0xA4 bytes
 addiu sp, sp, -0x18
 sw ra, 0x14 (sp)
 
-; check if the player pressed B on this frame
-.f_testInput BUTTON_B, BUTTON_PRESSED, proc802CB1C0_end
+; check if the player pressed L on this frame
+.f_testInput BUTTON_L, BUTTON_PRESSED, proc802CB1C0_end
 nop
 
-; check if mario is in the dive slide state, the slope slide state, or the slide recover state
+; check if mario is in the moving state
 li t0, MARIO_STRUCT
 lw t1, 0x0C(t0) ; get mario's current action
-li t2, ACTION_SLIDE
-sub t3, t1, t2
-li t2, ACTION_SLIDERECOVER
-sub t4, t1, t2
-li t2, ACTION_SLOPESLIDE
-and t6, t3, t4
-sub t5, t1, t2
-and t6, t6, t5
-bne t6, $zero, proc802CB1C0_end
+li t2, ACTION_MOVING
+bne t2, t1, proc802CB1C0_end
 nop
 
-; perform the dive hop
-li t1, ACTION_DIVE
+; perform the spin jump
+li t1, ACTION_FALL
 sw t1, 0x0C(t0) ; Set mario's action to diving
-li t1, 30.0
+li t1, 50.0
 mtc1 t1, f2
-swc1 f2, 0x4C(t0) ; Set mario's y-speed to 30.0
+swc1 f2, 0x4C(t0) ; Set mario's y-speed to 50.0
 
 ; end function
 proc802CB1C0_end:
